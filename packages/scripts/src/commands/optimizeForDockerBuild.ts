981652dc83b347b3dd0656c62cd7b537dae6cafd
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-09-13 10:34:17 +0900

    feat!: rename optimizeDepsForDocker to optimizeForDockerBuild

--- packages/scripts/src/commands/optimizeForDockerBuild.ts
+++ packages/scripts/src/commands/optimizeForDockerBuild.ts
@@ -1,16 +1,26 @@
 import child_process from 'node:child_process';
 import fs from 'node:fs/promises';
+import path from 'node:path';
 
 import type { CommandModule, InferredOptionTypes } from 'yargs';
 
-const builder = {} as const;
+const builder = {
+  'working-dir': {
+    description: 'A working directory',
+    type: 'string',
+    default: '.',
+    alias: 'w',
+  },
+} as const;
 
-export const optimizeSettingsForDocker: CommandModule<unknown, InferredOptionTypes<typeof builder>> = {
-  command: 'optimizeSettingsForDocker',
-  describe: 'Optimize devDependencies for building a Docker image',
+export const optimizeForDockerBuild: CommandModule<unknown, InferredOptionTypes<typeof builder>> = {
+  command: 'optimizeForDockerBuild',
+  describe: 'Optimize configuration when building a Docker image',
   builder,
-  async handler() {
+  async handler(argv) {
+    const workingDirectory = path.resolve(argv.workingDir);
     const opts = {
+      cwd: workingDirectory,
       stdio: 'inherit',
     } as const;
     child_process.spawnSync('yarn', ['config', 'set', 'enableTelemetry', '0'], opts);
@@ -25,7 +35,8 @@ export const optimizeSettingsForDocker: CommandModule<unknown, InferredOptionTyp
     child_process.spawnSync('yarn', ['plugin', 'set', 'remove', '@yarnpkg/plugin-typescript'], opts);
     child_process.spawnSync('yarn', ['plugin', 'set', 'remove', 'plugin-auto-install'], opts);
 
-    const packageJson = JSON.parse(await fs.readFile('package.json', 'utf8'));
+    const packageJsonPath = path.resolve(workingDirectory, 'package.json');
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'));
     const developmentDeps = packageJson.devDependencies;
     if (!developmentDeps) return;
 
@@ -57,6 +68,6 @@ export const optimizeSettingsForDocker: CommandModule<unknown, InferredOptionTyp
       }
     }
 
-    await fs.writeFile('package.json', JSON.stringify(packageJson));
+    await fs.writeFile(packageJsonPath, JSON.stringify(packageJson), 'utf8');
   },
 };
--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,13 +1,12 @@
 import { hideBin } from 'yargs/helpers';
 
 import { buildIfNeeded } from './commands/buildIfNeeded';
-import { optimizeSettingsForDocker } from './commands/optimizeSettingsForDocker';
+import { optimizeForDockerBuild } from './commands/optimizeForDockerBuild';
 
 // https://github.com/yargs/yargs/issues/1929#issuecomment-920391458
 // eslint-disable-next-line @typescript-eslint/no-var-requires, unicorn/prefer-module
 const yargs = require('yargs');
 
 export async function cli(): Promise<void> {
-  await yargs(hideBin(process.argv)).command(buildIfNeeded).command(optimizeSettingsForDocker).demandCommand().help()
-    .argv;
+  await yargs(hideBin(process.argv)).command(buildIfNeeded).command(optimizeForDockerBuild).demandCommand().help().argv;
 }
