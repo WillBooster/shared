30cbca15d02833947089c85bbd5081bd578d6c8e
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-10-18 21:14:10 +0900

    fix: add declaration files

--- packages/lib/rollup.config.mjs
+++ packages/lib/rollup.config.mjs
@@ -1,3 +1,3 @@
-import { getRollupConfig } from '@willbooster/rollup-configs/rollup.library.no-bundle.config.mjs';
+import { getRollupConfig } from '@willbooster/rollup-configs/rollup.library.noBundle.config.mjs';
 
 export default getRollupConfig('src/index.ts', 'package.json');
