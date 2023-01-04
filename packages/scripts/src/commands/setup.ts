385056a340309397b56a654de6228471595ce3ac
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-01-04 23:06:01 +0900

    fix: use inherit for non-parallel processes

--- packages/scripts/src/commands/setup.ts
+++ packages/scripts/src/commands/setup.ts
@@ -26,38 +26,50 @@ export const setup: CommandModule<unknown, InferredOptionTypes<typeof builder>> 
       await runCommand('asdf', ['install']);
     }
     if (dirents.some((d) => d.isFile() && d.name === 'pyproject.toml')) {
-      await promisePool.run(() => runCommand('poetry', ['config', 'virtualenvs.in-project', 'true']));
-      await promisePool.run(() => runCommand('poetry', ['config', 'virtualenvs.prefer-active-python', 'true']));
+      await runCommand('poetry', ['config', 'virtualenvs.in-project', 'true'], true);
+      await runCommand('poetry', ['config', 'virtualenvs.prefer-active-python', 'true'], true);
       const [, version] = child_process.execSync('asdf current python').toString().trim().split(/\s+/);
       await runCommand('poetry', ['env', 'use', version]);
       await runCommand('poetry', ['run', 'pip', 'install', '--upgrade', 'pip']);
       await runCommand('poetry', ['install', '--ansi']);
     }
+    await promisePool.promiseAllSettled();
   },
 };
 
-async function runCommand(command: string, args: string[]): Promise<void> {
+async function runCommand(command: string, args: string[], parallel = false): Promise<void> {
   console.info(chalk.green('Starting:'), command, args.join(' '));
 
-  await new Promise((resolve) => {
-    const proc = child_process.spawn(command, args, { stdio: 'pipe' });
-    let output = '';
-    proc.stdout.on('data', (data) => {
-      output += data;
-    });
-    proc.stderr.on('data', (data) => {
-      output += data;
-    });
-    proc.on('close', (code) => {
-      console.info(chalk.cyan('Finished:'), command, args.join(' '), `with exit code: ${code}`);
-      output = output.trim();
-      if (output) {
-        console.info('------------ start of output ------------');
-        console.info(output.trim());
-        console.info('------------- end of output -------------');
-        console.info();
-      }
-      resolve(undefined);
-    });
-  });
+  await (parallel
+    ? promisePool.run(
+        () =>
+          new Promise((resolve) => {
+            const proc = child_process.spawn(command, args, { stdio: 'pipe' });
+            let output = '';
+            proc.stdout.on('data', (data) => {
+              output += data;
+            });
+            proc.stderr.on('data', (data) => {
+              output += data;
+            });
+            proc.on('close', (code) => {
+              console.info(chalk.cyan('Finished:'), command, args.join(' '), `with exit code: ${code}`);
+              output = output.trim();
+              if (output) {
+                console.info('------------ start of output ------------');
+                console.info(output.trim());
+                console.info('------------- end of output -------------');
+                console.info();
+              }
+              resolve(undefined);
+            });
+          })
+      )
+    : new Promise((resolve) => {
+        const proc = child_process.spawn(command, args, { stdio: 'inherit' });
+        proc.on('close', (code) => {
+          console.info(chalk.cyan('Finished:'), command, args.join(' '), `with exit code: ${code}`);
+          resolve(undefined);
+        });
+      }));
 }
