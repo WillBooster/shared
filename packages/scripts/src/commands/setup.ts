b8734ad847d741e704f46c926749c8550e5491df
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-01-04 22:27:46 +0900

    .

--- packages/scripts/src/commands/setup.ts
+++ packages/scripts/src/commands/setup.ts
@@ -20,38 +20,44 @@ export const setup: CommandModule<unknown, InferredOptionTypes<typeof builder>> 
   command: 'setup',
   describe: 'Setup development environment',
   builder,
-  async handler(argv) {
+  async handler() {
     const dirents = await fs.readdir('.', { withFileTypes: true });
     if (dirents.some((d) => d.isFile() && d.name.includes('-version'))) {
       await runCommand('asdf', ['install']);
     }
     if (dirents.some((d) => d.isFile() && d.name === 'pyproject.toml')) {
+      await promisePool.run(() => runCommand('poetry', ['config', 'virtualenvs.in-project', 'true']));
+      await promisePool.run(() => runCommand('poetry', ['config', 'virtualenvs.prefer-active-python', 'true']));
+      const [, version] = child_process.execSync('asdf current python').toString().trim().split(/\s+/);
+      await runCommand('poetry', ['env', 'use', version]);
+      await runCommand('poetry', ['run', 'pip', 'install', '--upgrade', 'pip']);
       await runCommand('poetry', ['install', '--ansi']);
     }
   },
 };
 
 async function runCommand(command: string, args: string[]): Promise<void> {
-  console.info(chalk.green('Starting:'), command);
+  console.info(chalk.green('Starting:'), command, args.join(' '));
 
-  await promisePool.run(async () => {
+  await new Promise((resolve) => {
     const proc = child_process.spawn(command, args, { stdio: 'pipe' });
     let output = '';
-    await new Promise((resolve) => {
-      proc.stdout.on('data', (data) => {
-        output += data;
-      });
-      proc.stderr.on('data', (data) => {
-        output += data;
-      });
-      proc.on('close', (code) => {
-        console.info(chalk.cyan('Finished:'), command, `with exit code: ${code}`);
+    proc.stdout.on('data', (data) => {
+      output += data;
+    });
+    proc.stderr.on('data', (data) => {
+      output += data;
+    });
+    proc.on('close', (code) => {
+      console.info(chalk.cyan('Finished:'), command, args.join(' '), `with exit code: ${code}`);
+      output = output.trim();
+      if (output) {
         console.info('------------ start of output ------------');
         console.info(output.trim());
         console.info('------------- end of output -------------');
         console.info();
-        resolve(undefined);
-      });
+      }
+      resolve(undefined);
     });
   });
 }
