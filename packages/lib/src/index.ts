c9fcb7c8053119b719085223567a86c2410ed712
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-01-27 23:27:45 +0900

    fix: export spawnAsync()

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,3 +1,4 @@
-export { shuffle } from './shuffle.js';
 export { ignoreError, ignoreEnoent, ignoreErrorAsync, ignoreEnoentAsync } from './error.js';
+export { shuffle } from './shuffle.js';
+export { spawnAsync } from './spawn.js';
 export { zenkakuAlphanumericalsToHankaku } from './zenkaku.js';
