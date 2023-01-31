2601e8321bb3619f60fddc009e05ad74322863c2
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-01-31 22:06:58 +0900

    fix(optimizeForDockerBuild): remove more deps & scripts adding pre option

--- packages/scripts/src/commands/optimizeForDockerBuild.ts
+++ packages/scripts/src/commands/optimizeForDockerBuild.ts
@@ -1,64 +1,94 @@
 import child_process from 'node:child_process';
 import fs from 'node:fs/promises';
+import path from 'node:path';
 
 import type { CommandModule, InferredOptionTypes } from 'yargs';
 
 import { sharedOptions } from '../sharedOptions.js';
 
-const builder = { ...sharedOptions } as const;
+const builder = {
+  ...sharedOptions,
+  pre: {
+    description: 'Whether the optimization is before "docker build" or not',
+    type: 'boolean',
+    alias: 'p',
+  },
+} as const;
 
 export const optimizeForDockerBuild: CommandModule<unknown, InferredOptionTypes<typeof builder>> = {
   command: 'optimizeForDockerBuild',
   describe: 'Optimize configuration when building a Docker image',
   builder,
-  async handler() {
-    const opts = {
-      stdio: 'inherit',
-    } as const;
-    child_process.spawnSync('yarn', ['config', 'set', 'enableTelemetry', '0'], opts);
-    child_process.spawnSync('yarn', ['config', 'set', 'enableGlobalCache', '0'], opts);
-    child_process.spawnSync('yarn', ['config', 'set', 'nmMode', 'hardlinks-local'], opts);
-    const codes = ['YN0007', 'YN0013', 'YN0019'];
-    child_process.spawnSync(
-      'yarn',
-      ['config', 'set', 'logFilters', '--json', JSON.stringify(codes.map((code) => ({ code, level: 'discard' })))],
-      opts
-    );
-    child_process.spawnSync('yarn', ['plugin', 'remove', '@yarnpkg/plugin-typescript'], opts);
-    child_process.spawnSync('yarn', ['plugin', 'remove', 'plugin-auto-install'], opts);
+  async handler(argv) {
+    if (!argv.pre) {
+      const opts = {
+        stdio: 'inherit',
+      } as const;
+      child_process.spawnSync('yarn', ['config', 'set', 'enableTelemetry', '0'], opts);
+      child_process.spawnSync('yarn', ['config', 'set', 'enableGlobalCache', '0'], opts);
+      child_process.spawnSync('yarn', ['config', 'set', 'nmMode', 'hardlinks-local'], opts);
+      const codes = ['YN0007', 'YN0013', 'YN0019'];
+      child_process.spawnSync(
+        'yarn',
+        ['config', 'set', 'logFilters', '--json', JSON.stringify(codes.map((code) => ({ code, level: 'discard' })))],
+        opts
+      );
+      child_process.spawnSync('yarn', ['plugin', 'remove', '@yarnpkg/plugin-typescript'], opts);
+      child_process.spawnSync('yarn', ['plugin', 'remove', 'plugin-auto-install'], opts);
+    }
 
     const packageJson = JSON.parse(await fs.readFile('package.json', 'utf8'));
     const developmentDeps = packageJson.devDependencies;
     if (!developmentDeps) return;
 
+    const nameWordsToBeRemoved = [
+      'concurrently',
+      'conventional-changelog-conventionalcommits',
+      'eslint',
+      'husky',
+      'jest',
+      'kill-port',
+      'lint-staged',
+      'open-cli',
+      'playwright',
+      'prettier',
+      'pinst',
+      'sort-package-json',
+      'wait-on',
+      'semantic-release',
+      'vitest',
+    ];
+    if (!argv.pre) {
+      nameWordsToBeRemoved.push('@types', 'build-ts', 'rollup', 'typefest', 'typescript', 'vite', 'webpack');
+    }
     for (const name of Object.keys(developmentDeps)) {
-      const keywords = name.split(/[/@-]/);
       if (
-        keywords.includes('eslint') ||
-        keywords.includes('jest') ||
-        keywords.includes('playwright') ||
-        keywords.includes('prettier')
+        nameWordsToBeRemoved.some((word) => name.includes(word)) ||
+        (!argv.pre && name.includes('willbooster') && name.includes('config'))
       ) {
         delete developmentDeps[name];
-      } else if (name.includes('semantic-release') || (name.includes('willbooster') && name.includes('config'))) {
-        delete developmentDeps[name];
       }
     }
-    delete developmentDeps['conventional-changelog-conventionalcommits'];
-    delete developmentDeps['husky'];
-    delete developmentDeps['lint-staged'];
-    delete developmentDeps['open-cli'];
-    delete developmentDeps['pinst'];
-    delete developmentDeps['sort-package-json'];
-    delete developmentDeps['wait-on'];
 
+    const nameWordsOfUnnecessaryScripts = ['check', 'format', 'lint', 'start', 'test'];
+    const contentWordsOfUnnecessaryScripts = ['pinst ', 'husky '];
     const scripts = (packageJson.scripts || {}) as Record<string, string>;
-    for (const [key, value] of Object.entries(scripts)) {
-      if (value.startsWith('husky ') || value.startsWith('pinst ')) {
-        delete scripts[key];
+    for (const [name, content] of Object.entries(scripts)) {
+      if (
+        nameWordsOfUnnecessaryScripts.some((word) => name.includes(word)) ||
+        contentWordsOfUnnecessaryScripts.some((word) => content.includes(word))
+      ) {
+        delete scripts[name];
       }
     }
 
-    await fs.writeFile('package.json', JSON.stringify(packageJson), 'utf8');
+    if (argv.pre) {
+      await fs.mkdir('dist', { recursive: true });
+    }
+    await fs.writeFile(
+      argv.pre ? path.join('dist', 'package.json') : 'package.json',
+      JSON.stringify(packageJson),
+      'utf8'
+    );
   },
 };
