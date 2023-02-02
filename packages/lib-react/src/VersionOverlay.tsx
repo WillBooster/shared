a9c9946dcee479bd936e051e42a272a31f29dbae
taisii <85114269+taisii@users.noreply.github.com>
2023-02-02 10:53:01 +0900

    feat(lib-react): 印刷時にはVersionOverlayを非表示にする (#85)
    Co-authored-by: Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>

--- packages/lib-react/src/VersionOverlay.tsx
+++ packages/lib-react/src/VersionOverlay.tsx
@@ -1,18 +1,29 @@
 import type React from 'react';
 
+const css = `
+@media print {
+  .wb-shared-version {
+    display: none;
+  }
+}`;
+
 export const VersionOverlay: React.FC = () => (
-  <div
-    style={{
-      bottom: 4,
-      left: 4,
-      opacity: 0.5,
-      pointerEvents: 'none',
-      position: 'fixed',
-      userSelect: 'none',
-      fontSize: '0.75rem',
-    }}
-  >
-    {process.env.NEXT_PUBLIC_VERSION || process.env.VERSION || 'dev'} on{' '}
-    {process.env.NEXT_PUBLIC_ENVIRONMENT || process.env.ENVIRONMENT || 'local'}
-  </div>
+  <>
+    <style type="text/css">{css}</style>
+    <div
+      className="wb-shared-version"
+      style={{
+        bottom: 4,
+        left: 4,
+        opacity: 0.5,
+        pointerEvents: 'none',
+        position: 'fixed',
+        userSelect: 'none',
+        fontSize: '0.75rem',
+      }}
+    >
+      {process.env.NEXT_PUBLIC_VERSION || process.env.VERSION || 'dev'} on{' '}
+      {process.env.NEXT_PUBLIC_ENVIRONMENT || process.env.ENVIRONMENT || 'local'}
+    </div>
+  </>
 );
