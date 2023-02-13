952d534bae6867a756344f31bb2c7192bb8ff089
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-02-13 21:18:50 +0900

    .

--- packages/scripts/tests/buildIfNeeded.test.ts
+++ packages/scripts/tests/buildIfNeeded.test.ts
@@ -1,20 +1,29 @@
 import fs from 'node:fs';
 
-import { describe, expect, it, afterAll } from 'vitest';
+import { describe, expect, it, afterAll, beforeAll } from 'vitest';
 
 import { buildIfNeeded } from '../src/commands/buildIfNeeded.js';
 
 describe('buildIfNeeded', () => {
+  beforeAll(initializeFiles);
+
   it('app', async () => {
-    await fs.promises.rm('test-fixtures/app/node_modules', { recursive: true, force: true });
-    await fs.promises.writeFile('test-fixtures/app/index.js', `console.log('Hello');`);
     expect(await buildIfNeeded('yarn build', 'test-fixtures/app')).toBe(true);
     expect(await buildIfNeeded('yarn build', 'test-fixtures/app')).toBe(false);
+
     await fs.promises.writeFile('test-fixtures/app/index.js', `console.log('Hello'); console.log('Hello');`);
     expect(await buildIfNeeded('yarn build', 'test-fixtures/app')).toBe(true);
+    expect(await buildIfNeeded('yarn build', 'test-fixtures/app')).toBe(false);
+
+    await fs.promises.writeFile('test-fixtures/app/README.md', `# test-fixtures/app/`);
+    expect(await buildIfNeeded('yarn build', 'test-fixtures/app')).toBe(false);
   });
 
-  afterAll(async () => {
-    await fs.promises.writeFile('test-fixtures/app/index.js', `console.log('Hello');`);
-  });
+  afterAll(initializeFiles);
 });
+
+async function initializeFiles(): Promise<void> {
+  await fs.promises.rm('test-fixtures/app/node_modules', { recursive: true, force: true });
+  await fs.promises.writeFile('test-fixtures/app/index.js', `console.log('Hello');\n`);
+  await fs.promises.writeFile('test-fixtures/app/README.md', `# test-fixtures/app\n`);
+}
