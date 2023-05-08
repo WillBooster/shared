a611e203583cd1e90f079eba4085cf42439e49ad
remin <noreply@remin.jp>
2023-05-08 13:42:39 +0900

    feat(lib-blitz-next): Basic auth middleware for Blitz server

---
+++ packages/lib-blitz-next/tests/basicAuthMiddleware.test.ts
@@ -0,0 +1,58 @@
+import type { IncomingMessage, ServerResponse } from 'node:http';
+
+import type { MiddlewareNext } from 'blitz';
+import { beforeEach, describe, expect, test, vitest } from 'vitest';
+
+import { BasicAuthMiddleware } from '../src/index.js';
+
+describe('BasicAuthMiddleware', () => {
+  const basicAuthMiddleware = BasicAuthMiddleware({ password: 'password', realm: 'realm', username: 'username' });
+
+  let mockRequest: Partial<IncomingMessage> = {};
+  let mockResponse: Partial<ServerResponse> = {};
+  let mockNext: MiddlewareNext = vitest.fn();
+
+  beforeEach(() => {
+    mockRequest = {
+      headers: {},
+    };
+    mockResponse = {
+      setHeader: vitest.fn(),
+      end: vitest.fn(() => mockResponse as ServerResponse),
+    };
+    mockNext = vitest.fn();
+  });
+
+  test('authorized', () => {
+    mockRequest.headers = {
+      authorization: `Basic ${Buffer.from('username:password').toString('base64')}`,
+    };
+
+    basicAuthMiddleware(mockRequest as IncomingMessage, mockResponse as ServerResponse, mockNext);
+
+    expect(mockResponse.setHeader).toBeCalledTimes(0);
+    expect(mockNext).toBeCalledTimes(1);
+  });
+
+  test('incorrect password', () => {
+    mockRequest.headers = {
+      authorization: `Basic ${Buffer.from('username:incorrect').toString('base64')}`,
+    };
+
+    basicAuthMiddleware(mockRequest as IncomingMessage, mockResponse as ServerResponse, mockNext);
+
+    expect(mockResponse.setHeader).toBeCalledWith('WWW-Authenticate', "Basic realm='realm'");
+    expect(mockResponse.statusCode).toBe(401);
+    expect(mockResponse.end).toBeCalledWith('Unauthorized');
+    expect(mockNext).toBeCalledTimes(0);
+  });
+
+  test('no authorization header', () => {
+    basicAuthMiddleware(mockRequest as IncomingMessage, mockResponse as ServerResponse, mockNext);
+
+    expect(mockResponse.setHeader).toBeCalledWith('WWW-Authenticate', "Basic realm='realm'");
+    expect(mockResponse.statusCode).toBe(401);
+    expect(mockResponse.end).toBeCalledWith('Unauthorized');
+    expect(mockNext).toBeCalledTimes(0);
+  });
+});
