70de7c01d021553b17d6edd1877ccd5577b17acb
remin <noreply@remin.jp>
2023-05-18 13:18:02 +0900

    feat(lib-blitz-next): Basic auth middleware for Blitz server (#125)
    Co-authored-by: Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>

---
+++ packages/lib-blitz-next/src/middleware.ts
@@ -0,0 +1,31 @@
+import type { RequestMiddleware } from 'blitz';
+
+interface BasicAuthMiddlewareOptions {
+  password: string;
+  realm?: string;
+  username: string;
+}
+
+export function BasicAuthMiddleware(options: BasicAuthMiddlewareOptions): RequestMiddleware {
+  return async (request, response, next) => {
+    const authorizationHeader = request.headers.authorization ?? '';
+    const [type, encodedCredentials] = authorizationHeader.split(' ');
+    const credentials = Buffer.from(encodedCredentials ?? '', 'base64').toString();
+    const [requestUsername, requestPassword] = credentials.split(':');
+
+    if (
+      type !== 'Basic' ||
+      !requestUsername ||
+      !requestPassword ||
+      requestUsername !== options.username ||
+      requestPassword !== options.password
+    ) {
+      response.setHeader('WWW-Authenticate', `Basic realm='${options.realm || 'Secure Area'}'`);
+      response.statusCode = 401;
+      response.end('Unauthorized');
+      return;
+    }
+
+    await next();
+  };
+}
