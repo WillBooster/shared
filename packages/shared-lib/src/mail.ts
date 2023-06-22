4e57e4e76b150b4ea4be43dfc9afe4e9954037ab
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-06-22 16:09:09 +0900

    fix(shared-lib): refine text in mailTemplates

--- packages/shared-lib/src/mail.ts
+++ packages/shared-lib/src/mail.ts
@@ -10,7 +10,7 @@ class MailTemplates {
       from: 'no-reply@willbooster.dev',
       subject: 'パスワード再設定のご案内',
       text: `
-${appName}をご利用いただき誠にありがとうございます。
+${appName} をご利用いただき誠にありがとうございます。
 
 お客様からパスワード再設定のリクエストを受け取りました。
 以下のURLをクリックして、新しいパスワードを設定してください。
@@ -19,15 +19,14 @@ ${resetUrl}
 ${
   expirationDuration
     ? `
-なお、上記のURLは発行から約${expirationDuration}で失効します。
+上記のURLは発行から約${expirationDuration}で失効します。
 期限を過ぎてしまった場合や、URLが機能しない場合は、
 恐れ入りますが、再度パスワードを再設定してください。
 `
     : ''
 }
-引き続き${appName}をよろしくお願い申し上げます。
-
-${appName} サポートチーム
+なお、本メールアドレスは配信専用ですので、返信いただけません。
+引き続き ${appName} をよろしくお願い申し上げます。
     `.trim(),
     };
   }
