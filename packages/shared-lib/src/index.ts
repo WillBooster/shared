2f13f70f6c6fac0c70f11520e2b8d9bf3e1114c0
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-09-15 10:26:17 +0900

    fix: export RetryOptions interface (take 2)

--- packages/shared-lib/src/index.ts
+++ packages/shared-lib/src/index.ts
@@ -2,3 +2,5 @@ export { errorify, ignoreError, ignoreEnoent, ignoreErrorAsync, ignoreEnoentAsyn
 export { shuffle } from './shuffle.js';
 export { zenkakuAlphanumericalsToHankaku } from './zenkaku.js';
 export { mailTemplates } from './mail.js';
+
+export type { RetryOptions } from './error.js';
