76221d04bb530070354ef873b9fa7a61b5b718c9
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-09-26 13:51:24 +0900

    fix(shared-lib): export sleep()

--- packages/shared-lib/src/index.ts
+++ packages/shared-lib/src/index.ts
@@ -1,6 +1,7 @@
 export { errorify, ignoreError, ignoreEnoent, ignoreErrorAsync, ignoreEnoentAsync, withRetry } from './error.js';
-export { shuffle } from './shuffle.js';
-export { zenkakuAlphanumericalsToHankaku } from './zenkaku.js';
 export { mailTemplates } from './mail.js';
+export { shuffle } from './shuffle.js';
+export { sleep } from './sleep.js';
+export { zenkakuAlphanumericalsToHankaku } from './zenkaku.js';
 
 export type { RetryOptions } from './error.js';
