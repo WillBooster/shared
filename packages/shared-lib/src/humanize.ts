91b2f46439cf140875e42a1af1b8df04d621c440
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-05-23 22:50:36 +0900

    feat(lib): add humanizeNumber

---
+++ packages/shared-lib/src/humanize.ts
@@ -0,0 +1,13 @@
+export function humanizeNumber(value: number, { base = 1000, units = ['K', 'M', 'B', 'T', 'P'] } = {}): string {
+  if (value < base) {
+    return value.toString();
+  }
+
+  let unitIndex = -1;
+  while (value >= base && unitIndex < units.length - 1) {
+    value /= base;
+    unitIndex++;
+  }
+
+  return value.toFixed(2) + (unitIndex >= 0 ? units[unitIndex] : '');
+}
