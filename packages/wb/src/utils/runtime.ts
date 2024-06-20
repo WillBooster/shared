6bd72ec7438e17e322ee65f1ef0ee823be9e71d4
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-06-20 22:54:45 +0900

    fix: runtime detection

--- packages/wb/src/utils/runtime.ts
+++ packages/wb/src/utils/runtime.ts
@@ -1,4 +1,4 @@
-export const isRunningOnBun = process.argv[0].endsWith('/bun');
+export const isRunningOnBun = process.argv[0].endsWith('/bun') || process.env.npm_execpath?.endsWith('/bun');
 
 export const runtimeWithArgs = isRunningOnBun ? 'bun --bun run' : 'node';
 export const packageManagerWithArgs = isRunningOnBun ? 'bun --bun run' : 'yarn';
