efc8911b63d0698b74ac3ed78d70aba9f14f67f0
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-08-31 14:00:00 +0900

    fix(wb): disable --bun for 'next dev'

--- packages/wb/src/scripts/run.ts
+++ packages/wb/src/scripts/run.ts
@@ -162,5 +162,7 @@ function configureEnv(env: Record<string, string | undefined>, opts: Options): R
 }
 
 function fixBunCommand(command: string): string {
-  return command.includes('test/e2e-additional') ? command.replaceAll('bun --bun', 'bun') : command;
+  return command.includes('next dev') || command.includes('test/e2e-additional')
+    ? command.replaceAll('bun --bun', 'bun')
+    : command;
 }
