74fd794dbe549b7de94a2322e45c79185275a5ec
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-12-14 21:25:36 +0900

    fix: improve getConnectionLevelSqlitePragmas()

--- packages/shared-lib/src/sqlite.ts
+++ packages/shared-lib/src/sqlite.ts
@@ -1,5 +1,6 @@
 export function getConnectionLevelSqlitePragmas(): string {
-  return 'PRAGMA busy_timeout = 5000; PRAGMA synchronous = NORMAL; PRAGMA wal_autocheckpoint = 0;';
+  // cf. https://github.com/benbjohnson/litestream/issues/724#issue-3367254318
+  return 'PRAGMA busy_timeout = 5000; PRAGMA synchronous = NORMAL; PRAGMA wal_autocheckpoint = 10000;';
 }
 
 export function getPersistentSqlitePragmas(): string {
