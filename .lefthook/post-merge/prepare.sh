c22dd1b50590d0bd8d6bfc3aabc7be54c343e995
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2026-07-31 02:18:50 +0900

    chore: willboosterify this repo

--- .lefthook/post-merge/prepare.sh
+++ .lefthook/post-merge/prepare.sh
@@ -9,4 +9,5 @@ run_if_changed() {
 }
 
 run_if_changed "(mise\.toml|\.mise\.toml|\.tool-versions|\..+-version)" "mise install"
+if git diff --no-color -U0 ORIG_HEAD HEAD -- '*bunfig.toml' | grep --quiet -E '^[+-] *(globalStore|linker|publicHoistPattern)'; then rm -Rf -- 'node_modules' 'packages/shared-lib-blitz-next/node_modules' 'packages/shared-lib-next/node_modules' 'packages/shared-lib-node/node_modules' 'packages/shared-lib-react/node_modules' 'packages/shared-lib/node_modules' 'packages/wb/node_modules' 'packages/wbfy/node_modules'; fi
 run_if_changed "(package\.json|bun\.lock|bunfig\.toml|\.npmrc|patches/)" "bun install"
